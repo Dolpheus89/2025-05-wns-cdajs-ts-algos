2eee860d3e33ab6b4ec2b6dc55103afc7990412e
Florian <f.schaessens@gmail.com>
2024-07-02 14:57:25 +0200

    A4

--- src/challenges/A4/answer.ts
+++ src/challenges/A4/answer.ts
@@ -26,12 +26,44 @@
  * @returns Sorted list of days (only days with messages!) with a list of sorted messages of the day
  */
 
+
 // ↓ uncomment bellow lines and add your response!
-/*
+
 export default function ({ messages }: { messages: Message[] }): DayMessages[] {
-    return [];
+
+    let result:DayMessages[] = []
+
+    messages.map((message) => {
+        const day = message.sentAt.slice(0,11) + "00:00:00.000Z"
+        const messObj :DayMessages= {
+            day: day,
+            messages: []
+        }
+
+        const find = messages.filter((goodDay) => goodDay.sentAt.slice(0,11) === day.slice(0,11)).sort((a,b) => {
+            const dateA = new Date(a.sentAt).getTime()
+            const dateB = new Date(b.sentAt).getTime()
+    
+            return dateA - dateB
+        })
+        find.forEach((msg) => messObj.messages.push(msg ))
+        
+        const findDuplicate = result.find((days) => days.day === day)
+        if(!findDuplicate){
+            result.push(messObj)
+        }
+    })
+    
+    result.sort((a,b) => {
+        const dateA = new Date(a.day).getTime()
+        const dateB = new Date(b.day).getTime()
+
+        return dateA - dateB
+    })
+    
+    return result.sort();
 }
-*/
+
 
 // used interfaces, do not touch
 export interface Message {
